8e039777f193e8272ae02c7fac7df96c0d957f17
Alvaromg00 <alvaromoron27@gmail.com>
2024-07-30 01:16:00 +0200

    Estilos del tablero

--- projects/tic-tac-toe/src/App.jsx
+++ projects/tic-tac-toe/src/App.jsx
@@ -1,8 +1,20 @@
+import {Board} from './components/board';
+import './App.css';
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+
 function App() {
 
   return (
     <>
-      <h1>Tic-tac-toe</h1>
+    <h1 className='titlePage'>Tic-tac-toe</h1>
+    <div className='App'>
+      <Board />
+    </div>
     </>
   )
 }
---
+++ projects/tic-tac-toe/src/components/board.jsx
@@ -0,0 +1,22 @@
+import './board.css';
+
+export function Board () {
+
+
+
+    return (
+        <>
+            <div className='board'>
+                <div className='box'></div>
+                <div className='box'></div>
+                <div className='box'></div>
+                <div className='box'></div>
+                <div className='box'></div>
+                <div className='box'></div>
+                <div className='box'></div>
+                <div className='box'></div>
+                <div className='box'></div>
+            </div>
+        </>
+    )
+}
